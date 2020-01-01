a37427dfe3d6151654d10a68bafb026ce5161f76
user161760 <gabriel.sgaspar@yahoo.com.br>
2020-01-01 16:16:06 -0500

    Perguntas adicionadas e loop de questoes implementado

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        ["Four + Two is equl to Six","True"],
+        ["Five - Three is equal to One","False"],
+        ["Three + Eight is less than Ten","False"]
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Right!")
+        }else {
+            print("Wrong!")
+        }
+        
+        questionNumber += 1
+        
+        if questionNumber == quiz.count {
+           questionNumber = 0
+        }
+        
+        updateUI()
+        
+        
+    }
+    
+    func updateUI(){
+        questionLabel.text = quiz[questionNumber][0]
+    }
+    
 }
 
